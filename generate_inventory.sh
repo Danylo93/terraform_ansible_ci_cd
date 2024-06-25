038d3807d4f108494312e9403f998e2e8e58caaa
Danylo Oliveira <danylo.oliveira73@gmail.com>
2024-06-25 15:46:30 -0300

    yes we can

--- generate_inventory.sh
+++ generate_inventory.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
-# Verificar se os parâmetros foram passados corretamente
 if [ "$#" -ne 2 ]; then
-    echo "Uso: $0 <backend_instance_ip> <frontend_instance_ip>"
+    echo "Usage: $0 <backend_instance_ip> <frontend_instance_ip>"
     exit 1
 fi
 
-backend_ip=$1
-frontend_ip=$2
+BACKEND_IP=$1
+FRONTEND_IP=$2
 
-# Criar o diretório ansible/inventory se não existir
-mkdir -p ansible/inventory
-
-# Criar ou sobrescrever o arquivo hosts com as IPs fornecidas
 cat <<EOF > ansible/inventory/hosts
 [backend]
-$backend_ip
+$BACKEND_IP
 
 [frontend]
-$frontend_ip
+$FRONTEND_IP
 EOF
-
-echo "Arquivo de inventário gerado em ansible/inventory/hosts"
