3bc5683c9ae51eccb47197f793995b76df8c807f
Danylo Oliveira <danylo.oliveira73@gmail.com>
2024-06-25 15:28:20 -0300

    yes we can

--- generate_inventory.sh
+++ generate_inventory.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-# Execute o Terraform e capture as saídas
-cd terraform
-backend_ip=$(terraform output -raw backend_instance_ip)
-frontend_ip=$(terraform output -raw frontend_instance_ip)
-cd ..
+# Verificar se os parâmetros foram passados corretamente
+if [ "$#" -ne 2 ]; then
+    echo "Uso: $0 <backend_instance_ip> <frontend_instance_ip>"
+    exit 1
+fi
 
-# Crie o inventário do Ansible
-cat <<EOL > ansible/inventory/hosts
+backend_ip=$1
+frontend_ip=$2
+
+# Criar o diretório ansible/inventory se não existir
+mkdir -p ansible/inventory
+
+# Criar ou sobrescrever o arquivo hosts com as IPs fornecidas
+cat <<EOF > ansible/inventory/hosts
 [backend]
-backend_instance ansible_host=$backend_ip ansible_user=ubuntu
+$backend_ip
 
 [frontend]
-frontend_instance ansible_host=$frontend_ip ansible_user=ubuntu
-EOL
+$frontend_ip
+EOF
 
 echo "Arquivo de inventário gerado em ansible/inventory/hosts"
