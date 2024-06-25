5ee81467fdb6379d7344a573e02427e2e1e7ea4b
Danylo Oliveira <danylo.oliveira73@gmail.com>
2024-06-25 16:09:25 -0300

    ansible

--- generate_inventory.sh
+++ generate_inventory.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <backend_instance_ip> <frontend_instance_ip>"
-    exit 1
+backend_ip=$1
+frontend_ip=$2
+
+if [[ -z "$backend_ip" || -z "$frontend_ip" ]]; then
+  echo "Usage: $0 <backend_instance_ip> <frontend_instance_ip>"
+  exit 1
 fi
 
-BACKEND_IP=$1
-FRONTEND_IP=$2
-
-cat <<EOF > ansible/inventory/hosts
+cat <<EOL > ansible/inventory/hosts
 [backend]
-$BACKEND_IP
+$backend_ip
 
 [frontend]
-$FRONTEND_IP
-EOF
+$frontend_ip
+EOL
